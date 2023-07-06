0bd70f706c0a019dd18791e1394a0dc67e8fa75c
skyrichardson <86213662+skyrichardson@users.noreply.github.com>
2023-07-06 11:15:18 +1000

    listen with htmx hx-sse

--- server.py
+++ server.py
@@ -22,16 +22,9 @@ def listen():
     while True:
       global counter
       color = "red"
-      #document_path = os.getcwd() + 'color.txt'
-      # document = open(document_path, 'r')
-      #with open(document_path, "r") as f:
-        # color = f.read()
-        # print("******************")
-      if(color != "white"):
-        print(counter)
-        counter += 1
-        _data = json.dumps({"color":color, "counter":counter})
-        yield f"id: 1\ndata: {_data}\nevent: online\n\n"
+      print(counter)
+      counter += 1
+      yield f"id: 1\ndata: {counter}\nevent: online\n\n"
       time.sleep(0.5)
   return Response(respond_to_client(), mimetype='text/event-stream')
   
